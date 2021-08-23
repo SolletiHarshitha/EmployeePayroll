f447e7a2512b22d8b55e9aa0d700374ba6ed9203
Solleti Harshitha <solletiharshitha@gmail.com>
2021-08-23 23:16:14 +0530

    [Harshitha] Added-> Added Event Listener for name, date, salary

--- js/Form.js
+++ js/Form.js
@@ -1,18 +1,43 @@
-const salary = document.querySelector('#salary');
-const output = document.querySelector('.salary-output');
-output.textContent = salary.value;
-salary.addEventListener('input', function(){
-    output.textContent = salary.value;
-});
+window.addEventListener('DOMContentLoaded', (event) => {
+    const name = document.querySelector("#name");
+    const textError = document.querySelector(".text-error");
+    const nameRegex = RegExp("^[A-Z]{1}[a-z]{2,}$");
+    name.addEventListener('input', function(){
+        if(name.value.length == 0){
+            textError.textContent = "";
+            return;
+        }
+        try{
+            (new EmployeePayrollData()).name = name.value;
+            textError.textContent = "";
+        }
+        catch(e){
+            textError.textContent = e;
+        }
+    });
 
-const text = document.querySelector("#name");
-const textError = document.querySelector(".text-error");
-const nameRegex = RegExp("^[A-Z]{1}[a-z]{2,}$");
-text.addEventListener('input', function(){
-    if(nameRegex.test(text.value)) 
-    textError.textContent = "";
-    else
-    textError.textContent = "Name is Incorrect";
+    const salary = document.querySelector('#salary');
+    const output = document.querySelector('.salary-output');
+    output.textContent = salary.value;
+    salary.addEventListener('input', function(){
+    output.textContent = salary.value;
+    });
+
+    const startDate = document.querySelector("#startDate");
+    const day = document.getElementById("day").value;
+    const month = document.getElementById("month").value;
+    const year = document.getElementById("year").value;
+    const dateError = document.querySelector(".date-error");
+    startDate.addEventListener('input', async function(){
+        try{
+            (new EmployeePayrollData()).startDate = new Date(parseInt(year), parseInt(month), parseInt(day));
+            dateError.textContent = "";
+            console.log(day, month, year);
+        }
+        catch(e){
+            dateError.textContent = "Invalid Date";
+        }
+    });
 });
 
 function save(){
