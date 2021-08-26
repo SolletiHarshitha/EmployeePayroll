7e41853b447e4e74b5b5d11f818e8d33034fd7ca
Solleti Harshitha <solletiharshitha@gmail.com>
2021-08-26 22:56:55 +0530

    [Harshitha] Added-> On clicking the edit icon it displays the data in the form and we can edit and update it

--- js/Form.js
+++ js/Form.js
@@ -1,6 +1,6 @@
 window.addEventListener('DOMContentLoaded', (event) => {
     const name = document.querySelector("#name");
-    const textError = document.querySelector(".text-error");
+   // const textError = document.querySelector(".text-error");
     name.addEventListener('input', function(){
         if(name.value.length == 0){
             textError.textContent = "";
@@ -16,10 +16,12 @@ window.addEventListener('DOMContentLoaded', (event) => {
     });
 
     const salary = document.querySelector('#salary');
+    setTextValue('.salary-output',salary.value);
     const output = document.querySelector('.salary-output');
     output.textContent = salary.value;
     salary.addEventListener('input', function(){
     output.textContent = salary.value;
+    checkForUpdate();
     });
 
     const date = document.querySelector('#startDate');
